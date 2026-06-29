04c30f578894235e7aa054139fa338f52f595228
victor <vector.code11@gmail.com>
2026-06-29 11:09:08 +0000

    fix load_dataset

--- tomato_dl/utils/load_dataset.py
+++ tomato_dl/utils/load_dataset.py
@@ -9,7 +9,7 @@ def load_dataset(dataset_dir: os.PathLike,
                  *,
                  image_size: tuple[int, int] = (256, 256),
                  seed: int | None = None,
-                 batch_size: int | None = None,
+                 batch_size: int | None = 32,
                  split_ratio: tuple[float, float, float] = (0.7, 0.15, 0.15)) \
         -> list[tf.data.Dataset]:
 
@@ -40,19 +40,21 @@ def load_dataset(dataset_dir: os.PathLike,
             idx, [len(fnames[x]), 1])
         for idx, x in enumerate(classes)
     }
-
+   
     # Flatten
     fnames, labels = list(
         reduce(
-            lambda y, x: [y[0]+fnames[x], [y[1]+labels[x]]],
-            classes,
-            [[], []]
+            lambda y, x: [y[0]+fnames[x], tf.concat([y[1],labels[x]], axis=0)],
+            classes[1:],
+            [fnames[classes[0]], labels[classes[0]]]
         )
     )
 
-    def qget_labels(idxs): return itemgetter(idxs)(labels)
+    labels = labels.numpy().tolist()
 
-    def qget_fnames(idxs): return itemgetter(idxs)(fnames)
+    def qget_labels(idxs): return list(itemgetter(*idxs)(labels))
+
+    def qget_fnames(idxs): return list(itemgetter(*idxs)(fnames))
 
     total_count = len(labels)
     data_count = [int(total_count*x) for x in split_ratio]
@@ -60,12 +62,12 @@ def load_dataset(dataset_dir: os.PathLike,
     print(f"Total Size = {total_count}")
 
     train_idx, test_idx = train_test_split(
-        tf.range(data_count),
+       tf.range(total_count).numpy().tolist(),
         test_size=data_count[2],
         random_state=seed,
         stratify=labels
     )
-
+  
     train_idx, valid_idx = train_test_split(
         train_idx,
         test_size=data_count[1],
@@ -75,21 +77,22 @@ def load_dataset(dataset_dir: os.PathLike,
 
     def load_image(img_path: str):
         img = tf.io.read_file(img_path)
-        img = tf.image.decode_jpeg(img)
+        img = tf.image.decode_jpeg(img, channels=3)
         img = tf.image.resize(img, image_size)
+        tf.print(f"shape of IMG = {img.shape}")
         return img
 
     def make_dataset(idxs: list[int]):
         _fnames = qget_fnames(idxs)
         _labels = qget_labels(idxs)
-
+        
         X = tf.data.Dataset.from_tensor_slices(_fnames) \
             .map(load_image)
         y = tf.data.Dataset.from_tensor_slices(_labels)
         out = tf.data.Dataset.zip(X, y)
 
         if batch_size:
-            out.batch(batch_size)
+            out = out.batch(batch_size)
 
         out.class_names = classes
         out.file_paths = _fnames
